d1de5ad69dfcd88fc758e47c3c5237218a624479
Amit Nudel <72630488+AmitNudel@users.noreply.github.com>
2022-09-27 15:41:34 +0300

    Basic imp

---
+++ AutoVolumeControlCode/headers/tests.h
@@ -0,0 +1,9 @@
+#ifndef __TESTS__
+#define __TESTS__
+
+
+void PlaySound();
+void SoundTest();
+void Tests();
+
+#endif /* __TESTS_ */
---
+++ AutoVolumeControlCode/main/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#include "main_functions.h"
+#include "user_functions.h"
+#include "tests.h"
+
+int main()
+{
+    printf("AutoVolumeControl\n");
+        printf("\n");
+        /*     startWD();
+        */    
+        /*  while(1)
+            { */ 
+        ProgramRun();
+            /* } */
+        /*     stopWD();
+        */    
+    return 0;
+}
